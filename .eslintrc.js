783bf6ea5afdb0ac11e35d76a1fcc91189cb11aa
hell-llex <demeshenko333@gmail.com>
2022-05-06 20:07:06 +0300

    refactor: apply eslint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'airbnb-base',
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {
+		'no-plusplus': 'off',
+		'no-tabs': 'off',
+		'linebreak-style': 'off',
+		indent: [2, 'tab'],
+	},
+};
